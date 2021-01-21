3348f3355c69e8320b8da8d274b50e83fd229e8e
Kamesuta <kamesuta@gmail.com>
2021-01-21 09:58:18 +0900

    glfwtest

--- main.cpp
+++ main.cpp
@@ -5,24 +5,57 @@
 #include <stdlib.h>
 
 #include <mpv/client.h>
+#include <mpv/render_gl.h>
+#include <stdexcept>
+
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
 
 static inline void check_error(int status)
 {
-	if (status < 0) {
+	if (status < 0)
+	{
 		printf("mpv API error: %s\n", mpv_error_string(status));
 		exit(1);
 	}
 }
 
-int main(int argc, char *argv[])
+void* get_proc_address(void* ctx, const char* name)
 {
-	if (argc != 2) {
+	return (void*) glfwGetProcAddress(name);
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
 		printf("pass a single media file as argument\n");
 		return 1;
 	}
 
-	mpv_handle *ctx = mpv_create();
-	if (!ctx) {
+
+
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+
+	mpv_handle* ctx = mpv_create();
+	if (!ctx)
+	{
 		printf("failed creating context\n");
 		return 1;
 	}
@@ -38,16 +71,45 @@ int main(int argc, char *argv[])
 	check_error(mpv_initialize(ctx));
 
 	// Play this file.
-	const char *cmd[] = {"loadfile", argv[1], NULL};
+	const char* cmd[] = {"loadfile", argv[1], NULL};
 	check_error(mpv_command(ctx, cmd));
 
+	mpv_render_context* mpv_gl;
+
+	mpv_opengl_init_params gl_init_params{get_proc_address, nullptr, nullptr};
+	mpv_render_param params[]{
+			{MPV_RENDER_PARAM_API_TYPE,           const_cast<char*>(MPV_RENDER_API_TYPE_OPENGL)},
+			{MPV_RENDER_PARAM_OPENGL_INIT_PARAMS, &gl_init_params},
+			{MPV_RENDER_PARAM_INVALID,            nullptr}
+	};
+
+	if (mpv_render_context_create(&mpv_gl, ctx, params) < 0)
+		throw std::runtime_error("failed to initialize mpv GL context");
+
 	// Let it play, and wait until the user quits.
-	while (1) {
-		mpv_event *event = mpv_wait_event(ctx, 10000);
-		printf("event: %s\n", mpv_event_name(event->event_id));
-		if (event->event_id == MPV_EVENT_SHUTDOWN)
-			break;
+	//	while (1)
+	//	{
+	//		mpv_event* event = mpv_wait_event(ctx, 10000);
+	//		printf("event: %s\n", mpv_event_name(event->event_id));
+	//		if (event->event_id == MPV_EVENT_SHUTDOWN)
+	//			break;
+	//	}
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
 	}
+
+	glfwTerminate();
+
 
 	mpv_terminate_destroy(ctx);
 	return 0;
